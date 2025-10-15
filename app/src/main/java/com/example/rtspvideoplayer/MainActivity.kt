9b237d3372553d1928ecd16481e7102645255a9f
SimeonDoesStuffBG <simeondoesstuff@gmail.com>
2025-10-15 20:29:30 +0300

    feat: added video player to the app and ensured it can play a linked video

--- app/src/main/java/com/example/rtspvideoplayer/MainActivity.kt
+++ app/src/main/java/com/example/rtspvideoplayer/MainActivity.kt
@@ -1,59 +1,35 @@
 package com.example.rtspvideoplayer
 
+import android.widget.MediaController
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.example.rtspvideoplayer.databinding.ActivityMainBinding
+import android.widget.VideoView
+import androidx.core.net.toUri
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var videoPlayer: VideoView
+    private lateinit var mediaController: MediaController
+    private val rtspUri = "rtsp://dev.gradotech.eu:8554/stream"
+    private val videoUri= "https://videos.pexels.com/video-files/8392764/8392764-hd_1080_1920_25fps.mp4"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        setSupportActionBar(binding.toolbar)
+        videoPlayer = findViewById(R.id.videoPlayer)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        val uri = videoUri.toUri()
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
+        videoPlayer.setVideoURI(uri)
+
+        mediaController = MediaController(this)
+        mediaController.setAnchorView(videoPlayer)
+        mediaController.setMediaPlayer(videoPlayer)
+
+        videoPlayer.setMediaController(mediaController)
+        videoPlayer.start()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
