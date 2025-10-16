493c9aa18fb276b4e7421f51e3b33f0301f42532
SimeonDoesStuffBG <simeondoesstuff@gmail.com>
2025-10-16 12:41:56 +0300

    fix: Changed some settings to the player view item so that it plays the audio properly

--- app/src/main/java/com/example/rtspvideoplayer/MainActivity.kt
+++ app/src/main/java/com/example/rtspvideoplayer/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.rtspvideoplayer
 
-import android.media.browse.MediaBrowser
-import android.widget.MediaController
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.VideoView
 import androidx.core.net.toUri
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
@@ -15,9 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var videoPlayer: PlayerView
     private lateinit var player: ExoPlayer
-    private lateinit var mediaItem: MediaItem
     private val rtspUri = "rtsp://dev.gradotech.eu:8554/stream"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,16 +20,16 @@ class MainActivity : AppCompatActivity() {
 
         videoPlayer = findViewById(R.id.videoPlayer)
 
-        player = ExoPlayer.Builder(applicationContext).build()
+        player = ExoPlayer.Builder(this).build()
         videoPlayer.player = player
 
         val uri = rtspUri.toUri()
-        mediaItem = MediaItem.fromUri(uri)
+        val mediaItem = MediaItem.fromUri(uri)
 
         val mediaSource = RtspMediaSource.Factory().createMediaSource(mediaItem)
-
         player.setMediaSource(mediaSource)
-        player.prepare()
+
         player.playWhenReady = true
+        player.prepare()
     }
 }
